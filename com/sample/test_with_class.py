5acb96fc02f76997b4c77a01b173fd94e06009ff
shaina <Shainagupta9253@gmail.com>
2022-05-31 21:00:53 -0700

    Added a class with tests grouped together.

---
+++ com/sample/test_with_class.py
@@ -0,0 +1,15 @@
+import pytest
+
+# Test Class with name Demo
+## we can group tests in class and run them class by class too.
+import pytest
+
+class TestDemoClass:
+    val = 0
+    def test_one(self):
+        self.val += 1
+        assert self.val == 1
+
+    # this will fail since class variables are shared across functions
+    def test_two_negative(self):
+        assert self.val == 0
